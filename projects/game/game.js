155e7b1420325c6c95bd082584b70d57da9b7297
Grace Le <gl37702@email.vccs.edu>
2026-03-11 14:27:38 -0400

    asas

--- projects/game/game.js
+++ projects/game/game.js
@@ -17,7 +17,9 @@ function locationA() {
     function processInput(input){
         if (input.toLowerCase() === "xuanwu") {
             locationB();
-        } else {
+}else if(input.toLowerCase() === "stan country"){
+locationC();
+        }else {
             stayHere();
             waitThenCall(locationA);
         }
@@ -43,12 +45,35 @@ function locationB() {
     waitForInput(processInput);
 }
 
+function locationC(){
+clear();
+print("\nYou are in Stan Country");
+print("\nYou're greeted by a strange man in a robe and underwear, pointing a weapon at you !!");
+print("\nWhat do you do next?:" + "\n\tFight Him" + "\n\tTalk it out");
+
+function processInput(input){
+if (input.toLowerCase() === "fight him"){
+print("\nsdksdf");
+} else if(input.toLowerCase() === "talk it out"){
+print("\nBefore you can say anything, he fires at you! You have no choice but to fight now.");
+print("TIP: Check your inventory by typing 'inventory'!")
+if (input.toLowerCase() === "inventory"){
+print("Inventory:" + "\nScissors" + "\nThousand Demon Dagger")
+}
+} else {
+stayHere();
+waitThenCall(locationC);
+}
+}
+waitForInput(processInput);
+}
+
 function start(){
     print("Welcome to your journey to become the world's best assassin! Press any key to start");
-
+print("TIP: type 'Daibo Help!' for more information about an area!");
+start();
     function processInput(input){
             locationA();
     }
     waitForInput(processInput);
 }
-
