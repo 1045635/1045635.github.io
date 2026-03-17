f30642d9d9bbdcd702866ef8711ea99c68f3b9a5
Grace Le <gl37702@email.vccs.edu>
2026-03-16 21:13:01 -0400

    work on location B and location C

--- projects/game/game.js
+++ projects/game/game.js
@@ -2,6 +2,9 @@ let gameActive = true; //this variable is required.
                        //to stop the game, set it to false.
 
 let rank=17369
+weapon=0
+stanHealth=50
+
 
 //If you need, add any "helper" functions here
 
@@ -10,7 +13,7 @@ let rank=17369
 function locationA() {
     clear();
     print("\nYou are on Chicken Island!");
-    print("\nYour current rank is:" + rank);
+    print("\nYour current assassin rank is:" + rank);
     print("\nJourney to one of the following islands to defeat opponents and make your way up the ranks! Say one of these choices:" +
         "\n\tXuanwu" + "\n \tStan Country");
     
@@ -19,6 +22,8 @@ function locationA() {
             locationB();
 }else if(input.toLowerCase() === "stan country"){
 locationC();
+}else if(input.toLowerCase() === "daibo help!"){
+print("Chicken Island: You own a barbershop on this small island with your friends Dai Bo and Xiaofei. You've only lived here for a few years, but you can't remember anything from before. Type one of the areas above to travel and find out more about your origins.");
         }else {
             stayHere();
             waitThenCall(locationA);
@@ -32,11 +37,16 @@ function locationB() {
     print("\nWelcome to Xuanwu..");
     print("\nThis area feels familiar, for reasons unknown");
     print("\nWhere do you want to go next? Say one of these choices:" +
-        "\n\tChicken Island");
+        "\n\tChicken Islandi" + "\n\tcenter" + "\n\tAssassin Headquarters");
     
     function processInput(input){
         if (input.toLowerCase() === "chicken island") {
             locationA();
+}else if(input.toLowerCase() === "center");
+print("You enjoy the scenery and have some snacks with Dai bo and Xiaofei.");
+waitThenCall(locationB);
+}else if(input.toLowerCase()=== "Assassin Headquarters");
+print("Upon arrival, you see
         } else {
             stayHere();
             waitThenCall(locationB);
@@ -48,18 +58,45 @@ function locationB() {
 function locationC(){
 clear();
 print("\nYou are in Stan Country");
-print("\nYou're greeted by a strange man in a robe and underwear, pointing a weapon at you !!");
-print("\nWhat do you do next?:" + "\n\tFight Him" + "\n\tTalk it out");
+print("\nYou're greeted by a strange man in a robe and underwear, pointing a weapon at you.. Judging from his expression he recognizes you.");
+print("\nWhat do you do next?:" + "\n\tFight Him" + "\n\tTalk to him");
+
+function fightStan(input){
+print("\nChoose one of the following attacks: " + "\nstab" + "\nslash")
+if(input.toLowerCase() ==="stab"){
+stanHealth= stanHealth-30;
+print("Enemy's Current Health: " + str(stanHealth) + ")");
+fightStan(input);
+}else if(input.toLowerCase()==="slash");
+stanHealth=stanHealth-20;
+print("Enemy's Current Health: " + str(stanHealth) + ")");
+fightStan(input);
+} else{
+stayHere();
+
+if(stanHealth<= 0){
+rank= rank-7369
+print("Congrats! You beat the Prince of Stan, your rank has been boosted.");
+print("Current rank: " + str(rank) + ")");
+}
+waitThenCall(locationC);
+}
+}
+waitForInput(processInput);
+}
 
 function processInput(input){
 if (input.toLowerCase() === "fight him"){
-print("\nsdksdf");
-} else if(input.toLowerCase() === "talk it out"){
+fightStan(input);
+}else if(input.toLowerCase() === "daibo help!"){
+print("Stan Country: Home to the most technologically advanced nation who rely on AI, robots, and energy weapons; ruled by Prince Stan. Their lifestyle is significantly different from the residents of Xuanwu, making them enemies.");
+} else if(input.toLowerCase() === "talk to him"){
 print("\nBefore you can say anything, he fires at you! You have no choice but to fight now.");
-print("TIP: Check your inventory by typing 'inventory'!")
-if (input.toLowerCase() === "inventory"){
-print("Inventory:" + "\nScissors" + "\nThousand Demon Dagger")
-}
+print("Choose one of the options: " + "\ncontinue to fight" +  "\nflee")
+} else if(input.toLowerCase() === "continue to fight"){
+fightStan(input);
+}else if(input.toLowerCase()==="flee"){
+locationA();
 } else {
 stayHere();
 waitThenCall(locationC);
@@ -71,7 +108,6 @@ waitForInput(processInput);
 function start(){
     print("Welcome to your journey to become the world's best assassin! Press any key to start");
 print("TIP: type 'Daibo Help!' for more information about an area!");
-start();
     function processInput(input){
             locationA();
     }
