e3239556d396209d69aebb7595b168d703bc110e
Grace Le <gl37702@email.vccs.edu>
2026-03-26 14:56:45 -0400

    cube 2 and half of cube 3

--- practice/loops/loops.js
+++ practice/loops/loops.js
@@ -1,17 +1,35 @@
 let canvas = document.getElementById("canvas1");
 let ctx = canvas.getContext("2d");
 
-//fill in code for canvas1 here
+for(y = 0; y <= 800; y += 100){
+    for(let x = 25; x <= 725; x += 100){
+         ctx.fillRect(x, y, 50,25);
+    }
+}
 
 canvas = document.getElementById("canvas2");
 ctx = canvas.getContext("2d");
 
-//fill in code for canvas2 here
+let dx = 25;
+for(y=100; y<=800; y += 100){
+	for(let x=0; x<= dx ; x += 100){
+		ctx.fillRect(x,y,50,25);
+	} 
+dx += 100;
+}
 
 canvas = document.getElementById("canvas3");
 ctx = canvas.getContext("2d");
 
-//fill in code for canvas3 here
+dx=25;
+for(y=100; y<= 800; y += 100){
+	for(let x=0; x<=dx; x += 100){
+		ctx.fillRect(x,y,50,25);
+	}
+dx += 100;
+}
+
+
 
 canvas = document.getElementById("canvas4");
 ctx = canvas.getContext("2d");
